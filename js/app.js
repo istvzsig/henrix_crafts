1b0268238658b572a9575bf416869c74e0712268
iiistv <jinjash@gmail.com>
2021-01-29 08:14:26 +0700

    footer , css , javascript

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ function workshop() {
         // }
 
     } else {
-        span.textContent = ' Crafts'
+        span.textContent = ' Krafts'
     }
 
     if(folio_now < 300) {
@@ -38,13 +38,13 @@ function portfolio() {
     while(numberOfFolio < 6) {
         let image_container = document.createElement('div')
         let folioItem = document.createElement('div')
-        let galleryText = document.createElement('p')
+
         portfolio_gallery.append(folioItem)
         folioItem.append(image_container)
-        folioItem.append(galleryText)
+
         image_container.className = 'container_of_folio_item'
         folioItem.className = "folioItem"
-        galleryText.className = "galleryText"
+
         // galleryText.textContent = 'Fos'
         // folioItem.textContent = 'ASDSAD'
         numberOfFolio++
@@ -65,40 +65,79 @@ function portfolio() {
         'https://images.unsplash.com/photo-1610177498701-4f00c0bd1694?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8d29vZCUyMGNyYWZ0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'
 
     ]
+
     let imageList = document.querySelectorAll('.container_of_folio_item')
+    let folioItems = document.querySelectorAll('.folioItem')
+    let galleryText = document.createElement('p')
+
+    let imageText = [
+        'Image-1',
+        'Image-2',
+        'Image-3',
+        'Image-4',
+        'Image-5',
+        'Image-6',
+    ]
+
     imageList.forEach(item => {
+        let galleryText = document.createElement('p')
+        galleryText.id= 'galleryText'
         item === imageList[0] ?
-            item.style.backgroundImage = `url(${item_images[0]})`
+            (item.style.backgroundImage = `url(${item_images[0]})`,
+            galleryText.textContent = imageText[0],
+            folioItems[0].append(galleryText)
+        )
         : false
 
         item === imageList[1] ?
-            item.style.backgroundImage = `url(${item_images[1]})`
+            (item.style.backgroundImage = `url(${item_images[1]})`,
+            galleryText.textContent = imageText[1],
+            folioItems[1].append(galleryText)
+        )
         : false
 
         item === imageList[2] ?
-            item.style.backgroundImage = `url(${item_images[2]})`
+            (item.style.backgroundImage = `url(${item_images[2]})`,
+            galleryText.textContent = imageText[2],
+            folioItems[2].append(galleryText)
+        )
         : false
 
         item === imageList[3] ?
-            item.style.backgroundImage = `url(${item_images[3]})`
+            (item.style.backgroundImage = `url(${item_images[3]})`,
+            galleryText.textContent = imageText[3],
+            folioItems[3].append(galleryText)
+        )
         : false
 
         item === imageList[4] ?
-            item.style.backgroundImage = `url(${item_images[4]})`
+            (item.style.backgroundImage = `url(${item_images[4]})`,
+            galleryText.textContent = imageText[4],
+            folioItems[4].append(galleryText)
+        )
         : false
 
         item === imageList[5] ?
-            item.style.backgroundImage = `url(${item_images[5]})`
+            (item.style.backgroundImage = `url(${item_images[5]})`,
+            galleryText.textContent = imageText[5],
+            folioItems[5].append(galleryText)
+        )
         : false
+
+
+        item.style.filter = `blur(0) grayscale(100%)`
 
 
         item.onmouseover = function() {
             item.style.transform = `rotate(-10deg) scale(2)`
-            item.style.filter = `blur(10px) grayscale(100%)`
+            item.style.filter = `blur(0) grayscale(0)`
+            // galleryText.classList.add('showme')
+
         }
         item.onmouseleave = function() {
-            item.style.transform = `None`
-            item.style.filter = `None`
+            item.style.transform = `none`
+            item.style.filter = `blur(0) grayscale(100%)`
+            // galleryText.classList.remove('showme')
 
         }
 
@@ -113,6 +152,7 @@ function animations(){
         top_nav.style.background = `rgba(0,0,0,.9)`;
         // scroll_down_button.style.display = 'none'
         // logo.style.fontSize = `24px`
+        scroll_down_button.style.display = 'none'
     } else {
         top_nav.style.background = 'transparent'
         // logo.style.fontSize = `32px`
