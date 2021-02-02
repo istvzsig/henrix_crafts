8b16529ad3317230599312c743ab82e024b5b0e8
iiistv <jinjash@gmail.com>
2021-02-02 11:06:40 +0700

    portfolio

--- script/app.js
+++ script/app.js
@@ -3,11 +3,13 @@ const contactIcons = document.querySelectorAll('#top_nav a')
 function navAnim(x) {
     if(x >10) {
         // top_nav.style.background = `linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,0,0,.88) 50%,rgba(0,0,0,0) 100%)`
-        top_nav.style.background = 'black'
+        top_nav.style.background = `rgba(0,0,0,.88)`;
+        // top_nav.style.justifyContent = 'flex-end'
         contactIcons.forEach(c => {c.style.margin = `3px 10px`, c.style.padding = `0 5px`, c.style.opacity = 1})
     }
     else {
-        top_nav.style.background = `none`
+        top_nav.style.background = `none`;
+        // top_nav.style.justifyContent = 'center'
         contactIcons.forEach(c => {c.style.margin = `10px`, c.style.padding = `10px`, c.style.opacity = .6})
     }
 }
